27fddd16ea8020be55f278c3f49faefc82021ac1
Pao Her <p.her007@hotmail.com>
2022-04-08 18:46:10 -0500

    convert the app into a pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 
 module.exports = {
@@ -63,6 +64,27 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+        }),
+        // new, we are invoking a constructor function
+        // we instantiate our new WebpackPwaManifest, we provide an object as our only argument
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            start_url: "../index.html", // start_url property to specify the homepage for the pwa relative to the location of the manifest file
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            // fingerprints tell webpack whether or not it should generate unique fingerprints
+            // so that each time a new manifest is generated. It looks like this: manifest.lhge325d.json
+            // because we are not using fingerprints, we can also set inject to be false
+            fingerprints: false, 
+            inject: false,
+            icons: [{
+                src: path.resolve("assets/img/icons/icon-512x512.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons") // designate where the icons will be sent after the creation 
+                                                        // of the web manifest is completed by the plugin
+            }]
         })
     ],
     mode: 'development'
