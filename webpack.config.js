493a9ae567bf709147e1c1029295544353dfbe7d
Pao Her <p.her007@hotmail.com>
2022-04-08 17:48:20 -0500

    fixed offline functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,69 @@
-const webpack = require('webpack');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
-// const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-const config = {
-  entry: {
-    app: './assets/js/script.js',
-    events: './assets/js/events.js',
-    schedule: './assets/js/schedule.js',
-    tickets: './assets/js/tickets.js'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: `${__dirname}/dist`
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              esModule: false,
-              name(file) {
-                return '[path][name].[ext]';
-              },
-              publicPath(url) {
-                return url.replace('../', '/assets/');
-              }
+
+module.exports = {
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+
+    },
+    output: {
+       
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist',
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, './'),
+        },
+        compress: true,
+        port: 8080,
+      },
+  
+    module: {
+        rules: [
+            {
+                // using test property to find a regular expression
+                // we are trying to process any image file with the file extension of .jpg
+                // we could expand this expression to also search for other image file extension such as .png, .svg, .gif
+                test: /\.jpg$/i,
+                // use is where the actual loader is implemented
+                use:[
+                    {
+                        loader: 'file-loader',
+                        // options object that can rename our files and change the output path
+                        options: {
+                            esModule: false,
+                            // name function, which returns the name of the file with the file extension
+                            name(file){
+                                return "[path][name].[text]"
+                            },
+                            // a function that changes our assignment URL by replacing the ../ from our require() statement with /assets/
+                            publicPath: function(url){
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
             }
-          },
-          {
-            loader: 'image-webpack-loader'
-          }
         ]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static'
-    })
-    // new WebpackPwaManifest({
-    //   name: "Food Event",
-    //   short_name: "Foodies",
-    //   description: "An app that allows you to view upcoming food events.",
-    //   background_color: "#01579b",
-    //   theme_color: "#ffffff",
-    //   fingerprints: false,
-    //   inject: false,
-    //   icons: [{
-    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
-    //     sizes: [96, 128, 192, 256, 384, 512],
-    //     destination: path.join("assets", "icons")
-    //   }]
-    // })
-  ],
-  mode: 'development'
-};
-
-module.exports = config;
+    
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+        })
+    ],
+    mode: 'development'
+}
